36bae03f42b4c74692e6b692db347c062b93af24
fadd00 <andhikahutama9@gmail.com>
2025-10-16 17:04:28 +0700

    feat: Add padding to TataLetakColumn
    In `TataLetak.kt`, applies a `20.dp` padding modifier to the `Column` composable within the `TataLetakColumn` function.

--- app/src/main/java/com/sample/pertemuan3/TataLEtak.kt
+++ app/src/main/java/com/sample/pertemuan3/TataLEtak.kt
@@ -1,13 +1,15 @@
 package com.sample.pertemuan3
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun TataLetakColumn(modifier: Modifier) {
-    Column {
+    Column (modifier = modifier.padding(20.dp)){
         Text(text = "Komponen1")
         Text(text = "Komponen2")
         Text(text = "Komponen3")
